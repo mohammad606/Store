67915dbeeced52b3b40eebc264cd730886b96bd7
mohammad606 <59096227+mohammad606@users.noreply.github.com>
2023-10-14 21:19:10 +0300

    Update home.js
    Corrected the option to delete orders

--- pages/home.js
+++ pages/home.js
@@ -76,15 +76,14 @@ export default function Home() {
                         update(ref(data, `${user}/Input and output data/Output/${e.id}`), {
                             delet:true
                         })
-                        store.forEach((e)=>{
-                            items.forEach(f=>{
-                                qtn.forEach(q=>{
-                                    if(e.key == f && items.indexOf(f) == qtn.indexOf(q)){
-                                        update(ref(data,`${user}/Store/${e.key}`),{
-                                            qtn: Number(e.qtn) + Number(q)
-                                        });
-                                    }
-                                })
+                        const mergedArrasy = items.map((key, index) => ({'name' : key ,'qtn' : qtn[index]}))
+                        mergedArrasy.map(e=>{
+                            store.forEach(i=>{
+                                if(i.key === e.name ){
+                                    update(ref(data,`${user}/Store/${i.key}`),{
+                                        qtn: Number(i.qtn) + Number(e.qtn)
+                                    });
+                                }
                             })
                         })
                         setRelodData(1)
@@ -129,7 +128,7 @@ export default function Home() {
     //--------------------------------------------------------------------------- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     return (
         <>
-            <svg  style={{position:'absolute',top:'50px'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,224L11.4,213.3C22.9,203,46,181,69,181.3C91.4,181,114,203,137,229.3C160,256,183,288,206,272C228.6,256,251,192,274,170.7C297.1,149,320,171,343,186.7C365.7,203,389,213,411,192C434.3,171,457,117,480,128C502.9,139,526,213,549,213.3C571.4,213,594,139,617,138.7C640,139,663,213,686,250.7C708.6,288,731,288,754,282.7C777.1,277,800,267,823,250.7C845.7,235,869,213,891,218.7C914.3,224,937,256,960,250.7C982.9,245,1006,203,1029,192C1051.4,181,1074,203,1097,208C1120,213,1143,203,1166,202.7C1188.6,203,1211,213,1234,229.3C1257.1,245,1280,267,1303,229.3C1325.7,192,1349,96,1371,96C1394.3,96,1417,192,1429,240L1440,288L1440,0L1428.6,0C1417.1,0,1394,0,1371,0C1348.6,0,1326,0,1303,0C1280,0,1257,0,1234,0C1211.4,0,1189,0,1166,0C1142.9,0,1120,0,1097,0C1074.3,0,1051,0,1029,0C1005.7,0,983,0,960,0C937.1,0,914,0,891,0C868.6,0,846,0,823,0C800,0,777,0,754,0C731.4,0,709,0,686,0C662.9,0,640,0,617,0C594.3,0,571,0,549,0C525.7,0,503,0,480,0C457.1,0,434,0,411,0C388.6,0,366,0,343,0C320,0,297,0,274,0C251.4,0,229,0,206,0C182.9,0,160,0,137,0C114.3,0,91,0,69,0C45.7,0,23,0,11,0L0,0Z"></path></svg>
+            <svg  style={{position:'absolute',top:'50px'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fillOpacity="1" d="M0,224L11.4,213.3C22.9,203,46,181,69,181.3C91.4,181,114,203,137,229.3C160,256,183,288,206,272C228.6,256,251,192,274,170.7C297.1,149,320,171,343,186.7C365.7,203,389,213,411,192C434.3,171,457,117,480,128C502.9,139,526,213,549,213.3C571.4,213,594,139,617,138.7C640,139,663,213,686,250.7C708.6,288,731,288,754,282.7C777.1,277,800,267,823,250.7C845.7,235,869,213,891,218.7C914.3,224,937,256,960,250.7C982.9,245,1006,203,1029,192C1051.4,181,1074,203,1097,208C1120,213,1143,203,1166,202.7C1188.6,203,1211,213,1234,229.3C1257.1,245,1280,267,1303,229.3C1325.7,192,1349,96,1371,96C1394.3,96,1417,192,1429,240L1440,288L1440,0L1428.6,0C1417.1,0,1394,0,1371,0C1348.6,0,1326,0,1303,0C1280,0,1257,0,1234,0C1211.4,0,1189,0,1166,0C1142.9,0,1120,0,1097,0C1074.3,0,1051,0,1029,0C1005.7,0,983,0,960,0C937.1,0,914,0,891,0C868.6,0,846,0,823,0C800,0,777,0,754,0C731.4,0,709,0,686,0C662.9,0,640,0,617,0C594.3,0,571,0,549,0C525.7,0,503,0,480,0C457.1,0,434,0,411,0C388.6,0,366,0,343,0C320,0,297,0,274,0C251.4,0,229,0,206,0C182.9,0,160,0,137,0C114.3,0,91,0,69,0C45.7,0,23,0,11,0L0,0Z"></path></svg>
             <NavTow/>
             <div className={style.contAllSite}>
                 <NavBar/>
@@ -142,10 +141,10 @@ export default function Home() {
                             <div className={style.removeOrder}>
                                 {dataOrderSend?.map((e) => {
                                     const Items = [...e.items]
-                                    const qtn = [...e.qtn]
+                                    const qtn = e.qtn ? [...e.qtn] : []
                                     const arrIter = qtn[Symbol.iterator]();
                                     return (
-                                        <div className={e.delet?style.delet:style.contenarDataSending}>
+                                        <div className={e.delet?style.delet:style.contenarDataSending} key={e.key}>
                                             {e.delet? <div
                                                 className='position-absolute w-100 h-100 d-flex justify-content-center align-items-center'>
                                                 <strong className={style.deletetItemStrong}>Deleted</strong></div>
@@ -184,7 +183,7 @@ export default function Home() {
                                                     <p>client : {e.client}</p>
                                                     <p>Sender : {e.sender}</p>
                                                 </div>
-                                                <table key={e.key} className={style.order} style={{width: '100%'}}>
+                                                <table className={style.order} style={{width: '100%'}}>
 
                                                     <thead style={{
                                                         color: "#fff",
@@ -201,7 +200,7 @@ export default function Home() {
                                                         Items?.map(f => {
 
                                                             return (
-                                                                <tr>
+                                                                <tr key={f.key}>
                                                                     <td>{f}</td>
                                                                     <td>{arrIter.next().value}</td>
                                                                 </tr>
@@ -209,7 +208,7 @@ export default function Home() {
                                                         })
 
                                                     }
-                                                    <tr ><td colspan="2">pieces : {e.allQtn}</td></tr>
+                                                    <tr ><td colSpan="2">pieces : {e.allQtn}</td></tr>
 
                                                     </tbody>
 
@@ -228,7 +227,7 @@ export default function Home() {
                             <div className={style.AddItem}>
                                 {dataAddToStore?.map((e) => {
                                     return e ? (
-                                        <div className={e.delet?style.delet:style.contenarDataSending}>
+                                        <div className={e.delet?style.delet:style.contenarDataSending} key={e.key}>
                                             {e.delet?
                                                 <div
                                                 className='position-absolute w-100 h-100 d-flex justify-content-center align-items-center'>
@@ -282,38 +281,3 @@ export default function Home() {
 
     )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
